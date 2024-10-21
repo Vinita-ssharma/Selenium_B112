5a1e4473f08bbbbc19d18f82aa971c033ca16c1e
Vinita-ssharma <vinita96sharma@gmail.com>
2024-10-21 12:11:18 +0530

    Initial draft

---
+++ src/test/java/script/Demo1.java
@@ -0,0 +1,20 @@
+package script;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import generic.BaseTest;
+import generic.Utility;
+
+public class Demo1 extends BaseTest{
+
+	@Test
+	public void testA()
+	{
+		String data = Utility.getXLCellData(XL_PATH,"Sheet1",0,0);
+		test.info("Data from excel: "+data);
+		String title = driver.getTitle();
+		test.info("testA"+title);
+		
+	}
+}
